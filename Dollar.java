6ab513a85761e26a0faf6e7012131d6d4b7901ae
perfil <arthureich@hotmail.com>
2023-06-20 16:59:02 -0300

    Classe Dollar, Main e teste

--- Dollar.java
+++ Dollar.java
@@ -1,11 +1,5 @@
-class Dollar {
+ class Dollar {
        int amount = 10;
        Dollar(int amount) {}			
        void times(int multiplier) {}
     }	
-
-    public void testMultiplication() {
-       Dollar five = new Dollar(5);
-       five.times(2);
-       assertEquals(10, five.amount);
-    }
