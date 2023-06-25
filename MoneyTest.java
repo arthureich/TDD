228459527670dac105c856e5c1fdc09ae3237c8d
arthureich <128417915+arthureich@users.noreply.github.com>
2023-06-24 23:48:40 -0300

    Update MoneyTest.java

--- MoneyTest.java
+++ MoneyTest.java
@@ -1,12 +1,26 @@
-
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
-public class DollarTest {
-  
-  public void testMultiplication() {
-       Dollar five = new Dollar(5);
-       five.times(2);
-       assertEquals(10, five.amount);
-    }
+public class MoneyTest  {
+
+@Test
+public void testMultiplication() {
+   Money five = Money.dollar(5);
+   assertEquals(Money.dollar(10), five.times(2));
+   assertEquals(Money.dollar(15), five.times(3));
 }
+@Test
+public void testEquality() {
+   assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
+   assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+   assertFalse(Money.franc(5).equals(Money.dollar(5)));
+}
+@Test
+public void testCurrency() {
+   assertEquals("USD", Money.dollar(1).currency());
+   assertEquals("CHF", Money.franc(1).currency());
+}
+}
+
